4893cf5a1ac82bec6e571ad4c46b065ab8f2d50d
Eli Mallon <eli@livepeer.org>
2021-06-09 20:09:30 -0700

    working token generation

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,14 @@ FROM livepeerci/api:master as api
 FROM livepeerci/www:master as www
 FROM livepeer/streamtester:mist-api-connector as mist-api-connector
 
+FROM golang as unpack
+WORKDIR /app
+ADD go.mod go.mod
+ADD go.sum go.sum
+RUN go mod download
+ADD unpack-box.go unpack-box.go
+RUN go build unpack-box.go
+
 # We want to inherit from a CUDA container for driver-support... so let's just use go-livepeer,
 # which already has a CUDA environment.
 FROM livepeer/go-livepeer:master
@@ -35,6 +43,7 @@ COPY traefik-routes.toml /traefik-routes.toml
 
 RUN npm install -g serve
 COPY --from=www /www /www
+COPY --from=unpack /app/unpack-box /usr/bin/unpack-box
 
 ENTRYPOINT []
 CMD supervisord -c /usr/local/supervisord.conf
--- unpack-box.go
+++ unpack-box.go
@@ -13,32 +13,82 @@ import (
 	"golang.org/x/crypto/pbkdf2"
 )
 
-func main() {
-	url := "http://10.173.0.1:8080/api/user"
-	fmt.Println("URL:>", url)
-
-	salt, _ := hex.DecodeString("69195A9476F08546")
-	dk := pbkdf2.Key([]byte("password"), salt, 10000, 32, sha256.New)
-	hash := strings.ToUpper(hex.EncodeToString(dk))
-	var body = map[string]string{
-		"email":    "admin@livepeer.dev",
-		"password": hash,
+func post(url, authorization string, body map[string]string) ([]byte, error) {
+	b, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
 	}
-	b, _ := json.Marshal(body)
 	fmt.Println(string(b))
 
 	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(b))
 
 	client := &http.Client{}
 	req.Header.Add("Content-Type", `application/json`)
+	if authorization != "" {
+		req.Header.Add("Authorization", authorization)
+	}
 	resp, err := client.Do(req)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	defer resp.Body.Close()
 
-	fmt.Println("response Status:", resp.Status)
-	fmt.Println("response Headers:", resp.Header)
-	res, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println("response Body:", string(res))
+	if resp.StatusCode != 201 {
+		return nil, fmt.Errorf("error: http response %s", resp.Status)
+	}
+	res, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+type JWTResp struct {
+	Token string `json:token`
+}
+
+type APITokenResp struct {
+	ID string `json:id`
+}
+
+func main() {
+	url := "http://127.0.0.1:80/api/user"
+	fmt.Println("URL:>", url)
+
+	salt, err := hex.DecodeString("69195A9476F08546")
+	if err != nil {
+		panic(err)
+	}
+	dk := pbkdf2.Key([]byte("password"), salt, 10000, 32, sha256.New)
+	hash := strings.ToUpper(hex.EncodeToString(dk))
+	var body = map[string]string{
+		"email":    "admin@livepeer.dev",
+		"password": hash,
+	}
+	_, err = post("http://127.0.0.1:80/api/user", "", body)
+	if err != nil {
+		panic(err)
+	}
+	jwtBytes, err := post("http://127.0.0.1:80/api/user/token", "", body)
+	if err != nil {
+		panic(err)
+	}
+	var jwt JWTResp
+	err = json.Unmarshal(jwtBytes, &jwt)
+	if err != nil {
+		panic(err)
+	}
+	var apiTokenBody = map[string]string{
+		"name": "System Token - DO NOT DELETE",
+	}
+	tokenBytes, err := post("http://127.0.0.1:80/api/api-token", fmt.Sprintf("JWT %s", jwt.Token), apiTokenBody)
+	if err != nil {
+		panic(err)
+	}
+	var token APITokenResp
+	err = json.Unmarshal(tokenBytes, &token)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(token.ID)
 }
