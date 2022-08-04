3bc5b79ccbde0f35ade836a222e8b98acd111f39
Thom Shutt <thomshutt@users.noreply.github.com>
2022-08-04 16:57:06 +0100

    Revert logging changes

--- cmd/downloader/utils/utils.go
+++ cmd/downloader/utils/utils.go
@@ -84,19 +84,18 @@ func IsCleanupFile(name string) bool {
 }
 
 func DownloadFile(path, url string, skipDownloaded bool) error {
-	glog.Infof("Downloading %s", url)
+	glog.V(9).Infof("Downloading %s", url)
 	if skipDownloaded && IsFileExists(path) {
 		glog.Infof("File already downloaded. Skipping!")
 		return nil
 	}
-	url = url + "?cachebust=true"
 	resp, err := http.Get(url)
 	if err != nil {
 		return err
 	}
-	glog.Infof("Response statusCode=%d", resp.StatusCode)
+	glog.V(9).Infof("Response statusCode=%d", resp.StatusCode)
 	if resp.StatusCode != 200 {
-		return fmt.Errorf("HTTP %d while downloading %q", resp.StatusCode, url)
+		return fmt.Errorf("HTTP %d while downloading %s", resp.StatusCode, url)
 	}
 	defer resp.Body.Close()
 	tempPath := fmt.Sprintf("%s.TEMP", path)
