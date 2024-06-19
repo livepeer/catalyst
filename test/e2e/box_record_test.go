7c22f4ce319422046454e47cc62099695d06b891
Victor Elias <victorgelias@gmail.com>
2024-06-19 18:24:56 +0100

    e2e: Give box up to 3 minutes to start
    Improves local runs at least

--- test/e2e/box_record_test.go
+++ test/e2e/box_record_test.go
@@ -52,7 +52,7 @@ func startBoxWithEnv(ctx context.Context, t *testing.T, hostname, network string
 		Networks:     []string{network},
 		ExposedPorts: []string{"1935:1935/tcp", "8888:8888/tcp"},
 		ShmSize:      1000000000,
-		WaitingFor:   wait.NewLogStrategy("API server listening"),
+		WaitingFor:   wait.NewLogStrategy("API server listening").WithStartupTimeout(3 * time.Minute),
 		Env: map[string]string{
 			"LP_API_FRONTEND": "false",
 		},
